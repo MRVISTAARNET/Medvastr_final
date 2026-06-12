89cc5ff5b6de5354e92be65eb1d8338a3ab983cd
Admin <mrvistaar@gmail.com>
2026-06-12 20:27:48 +0530

    test: add test-page to verify deployment

---
+++ src/app/(shop)/test-page/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+        <div style={{ padding: "100px", textAlign: "center" }}>
+            <h1>Website Update Successful! 🟢</h1>
+            <p>If you see this, it means your website is updating correctly.</p>
+        </div>
+    );
+}
